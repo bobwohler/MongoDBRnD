a4360d0d2d0cd0efecaa6ae32a040cfea2f256a1
Bob Wohler <bobwohler@gamestop.com>
2013-03-22 11:40:09 -0500

    Test reorgs
    Make tests repeatable and addition of new tests

--- MongoDBRnD/MongoDBTests.cs
+++ MongoDBRnD/MongoDBTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MongoDB.Driver;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MongoDB.Bson;
-using System.Collections.Generic;
+using MongoDB.Driver;
+using System;
 
 namespace MongoDBRnD
 {
@@ -11,10 +10,12 @@ namespace MongoDBRnD
     {
         private MongoClient _mongoClient = null;
         private MongoDatabase _database = null;
+        // These tests use a MongoDB server instance hosted in an Azure IaaS VM.
         private string _connectionString = "mongodb://mongodbsvr.cloudapp.net:27017";
         private string _dbName = "test";
         private string _collectionName = "foo";
-        private Guid _fooId = Guid.Parse("1A886002-E3A9-4983-A7CA-354B8BD4DE53");
+        private Guid _bobFooId = Guid.Parse("1A886002-E3A9-4983-A7CA-354B8BD4DE53");
+        private Guid _hollyFooId = Guid.Parse("5610F094-1ECA-4444-97C6-FEA231B8FC4F");
 
         [TestInitialize()]
         public void Initialize()
@@ -22,33 +23,102 @@ namespace MongoDBRnD
             _mongoClient = new MongoClient(_connectionString);
             MongoServer server = _mongoClient.GetServer();
             _database = server.GetDatabase(_dbName);
+            _database.GetCollection<Foo>(_collectionName).RemoveAll();
         }
-
-        //TODO: Create test that inserts the two rows that are in the remote db by default
 
         [TestMethod]
-        public void Get_All_Foo()
+        public void Add_Foo()
         {
+
+            #region Test setup
             MongoCollection<Foo> fooCollection = _database.GetCollection<Foo>(_collectionName);
-            Assert.AreEqual(fooCollection.FindAll().Count(),2);
+            fooCollection.RemoveAll();
+            #endregion
+
+            BsonDocument fooDocEthan = new BsonDocument {
+                { "_id", Guid.NewGuid().ToString() },
+                { "name", "Ethan"}
+            };
+            BsonDocument fooDocAbby = new BsonDocument {
+                { "_id", Guid.NewGuid().ToString() },
+                { "name", "Abby" }
+            };
+            
+            fooCollection.Insert(fooDocEthan);
+            fooCollection.Insert(fooDocAbby);
+
+            Assert.AreEqual(fooCollection.FindAll().Count(), 2);
+
+            #region Test cleanup
+            fooCollection.RemoveAll();
+            #endregion
+
         }
+
+        //[TestMethod]
+        //public void Get_All_Foo()
+        //{
+        //    MongoCollection<Foo> fooCollection = _database.GetCollection<Foo>(_collectionName);
+        //    Assert.AreEqual(fooCollection.FindAll().Count(),2);
+        //}
 
         [TestMethod]
         public void Get_Foo_ByID()
         {
+            #region Test setup
             MongoCollection<Foo> fooCollection = _database.GetCollection<Foo>(_collectionName);
-            QueryDocument query = new QueryDocument("_id", "1A886002-E3A9-4983-A7CA-354B8BD4DE53");
+            fooCollection.RemoveAll();
+            BsonDocument fooDocEthan = new BsonDocument {
+                { "_id", _bobFooId },
+                { "name", "Bob"}
+            };
+            BsonDocument fooDocAbby = new BsonDocument {
+                { "_id", _hollyFooId },
+                { "name", "Holly" }
+            };
+
+            fooCollection.Insert(fooDocEthan);
+            fooCollection.Insert(fooDocAbby);
+            #endregion
+
+            QueryDocument query = new QueryDocument("_id", _bobFooId);
             Foo foo = fooCollection.FindOne(query);
             Assert.AreEqual(foo.Name, "Bob");
+            
+            #region Test cleanup
+            fooCollection.RemoveAll();
+            #endregion
+
         }
 
         [TestMethod]
         public void Get_Foo_ByName()
         {
+            #region Test setup
             MongoCollection<Foo> fooCollection = _database.GetCollection<Foo>(_collectionName);
+            fooCollection.RemoveAll();
+            BsonDocument fooDocBob = new BsonDocument {
+                { "_id", _bobFooId },
+                { "name", "Bob"}
+            };
+            BsonDocument fooDocHolly = new BsonDocument {
+                { "_id", _hollyFooId },
+                { "name", "Holly" }
+            };
+
+            fooCollection.Insert(fooDocBob);
+            fooCollection.Insert(fooDocHolly);
+            #endregion
+            
             QueryDocument query = new QueryDocument("name", "Holly");
             Foo foo = fooCollection.FindOne(query);
             Assert.AreEqual(foo.Id, Guid.Parse("5610F094-1ECA-4444-97C6-FEA231B8FC4F"));
+
+            #region Test cleanup
+            fooCollection.RemoveAll();
+            #endregion
+
         }
+
     }
 }
